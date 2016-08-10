afcd06ab0afd42aa3d9863d63133220494e0ff46
alejandro ludueña <aleludue@hotmail.com>
2016-08-10 11:12:56 -0300

    - Version inicial del modelo de datos - Configuaracion base de datos

--- prepago/settings.py
+++ prepago/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'prepago.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'energia_prepaga',
+        'USER': 'energia_prepaga',
+        'PASSWORD': 'pr3p4g4',
+        'HOST': '192.168.1.2',
+        'PORT': '3306',
     }
 }
 
--- prepapp/models.py
+++ prepapp/models.py
@@ -34,8 +34,60 @@ class Terreno(models.Model):
     condicionIva = models.CharField(max_length=4, choices=CONDICION_IVA_CHOICES)
     nroMedidorEnergia = models.CharField(max_length=30)
     activo = models.BooleanField(default=True)
+    cargoConsumoAgua = models.BooleanField(default=True)
 
 class RecambioMedidor(models.Model):
     terreno = models.ForeignKey(Terreno)
     fecha = models.DateField(default=timezone.now())
     nroMedidor = models.CharField(max_length=30)
+
+class Tarifa(models.Model):
+    nombre = models.CharField(max_length=30)
+    terreno = models.ForeignKey(Terreno)
+
+class EscalonesEnergia(models.Model):
+    tarifa = models.ForeignKey(Tarifa)
+    desde = models.IntegerField()
+    hasta = models.IntegerField()
+    valor = models.FloatField()
+
+class Items(models.Model):
+    TIPOS_CHOICES = (('FIJ', 'Fijo'),
+                    ('VAR', 'Variable'))
+
+    APLICACION_CHOICES = (('CF', 'Cargo Fijo'),
+                          ('EN', 'Energia'))
+
+    nombre = models.CharField(max_length=60)
+    tipo = models.CharField(max_length=3, choices=TIPOS_CHOICES)
+    aplicacion = models.CharField(max_length=2, choices=APLICACION_CHOICES)
+    valor = models.FloatField()
+    tarifa = models.ManyToManyField(Tarifa, through='Gravamen')
+
+class Gravamen(models.Model):
+    IVA_CHOICES = (('IVA21', 'IVA 21%'),
+                   ('IVA27', 'IVA 27%'),
+                   ('NOGRA', 'No Gravado'),
+                   ('EXENT', 'Exento'))
+
+    tarifa = models.ForeignKey(Tarifa)
+    item = models.ForeignKey(Items)
+    iva = models.CharField(max_length=5, choices=IVA_CHOICES)
+
+class Cesp(models.Model):
+    nroCesp = models.IntegerField(primary_key=True)
+    fecha = models.DateField()
+
+class Factura(models.Model):
+    nroFactura = models.CharField(max_length=50, primary_key=True)
+    fecha = models.DateField(auto_now_add=True)
+    nroTerreno = models.ForeignKey(Terreno)
+    importe = models.FloatField()
+    cesp = models.ForeignKey(Cesp)
+    tarifa = models.ForeignKey(Tarifa)
+    items = models.ManyToManyField(Items, through='Factura_Items')
+
+class Factura_Items(models.Model):
+    nroFactura = models.ForeignKey(Factura)
+    item = models.ForeignKey(Items)
+
